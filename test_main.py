f96c005ebb8d3d2055c4c9bda876a3a745f2c74c
Luan Sinani <32123956+Luan-Sinani@users.noreply.github.com>
2020-05-06 17:24:46 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,5 +1,6 @@
 '''
 Tests for jwt flask app.
+https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
 '''
 import os
 import json
